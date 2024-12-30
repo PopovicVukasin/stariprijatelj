5ca33050a6bf77be2a6354e059420cbff60109b5
PopovicVukasin <119617947+PopovicVukasin@users.noreply.github.com>
2024-12-30 16:12:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-<script>
-    document.querySelector(".puzzle").addEventListener("click", () => {
-        alert("You found it! The secret word is 'Love'. 💕");
-    });
-</script>
+document.querySelector(".puzzle").addEventListener("click", () => {
+    alert("You found it! The secret word is 'Love'. 💕");
+});
+
